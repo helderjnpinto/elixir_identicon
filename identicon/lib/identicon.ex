2121231ceca8575a36183730ec2558f4bac231a9
Helder <helder@yarilabs.com>
2017-12-01 17:13:35 +0000

    hash string and main fn added

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,18 +1,16 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for Identicon.
+        Documentation for Identicon.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_string
   end
+
+  def hash_string(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+  end
+
 end
